3b0c18be85506bc7a5e8bfb07a8adac9dcfee7ff
dmsnet <dmytro.mostiv@gmail.com>
2016-06-01 14:07:07 +0300

    ttt

--- constants.py
+++ constants.py
@@ -2,7 +2,7 @@ __author__ = 'dmsnet'
 # -*- coding: utf-8 -*-
 
 pay_cur= {'UAH':'980','RUB':'643'}
-conn_string="host='95.46.98.30' dbname='db_test' user='postgreadmin' password='pomason3'"
+conn_string="host='localhost' dbname='db_test' user='postgreadmin' password='passs'"
 secret ="zvDcFdLj7YiM0ODUUBa2cpGmcpHXqR7CF"
 shop_id=300966
 
